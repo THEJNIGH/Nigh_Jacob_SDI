8c7788a37177493806042257cfb70739b6b08eb4
jacobnigh <jacobnigh@hotmail.com>
2015-01-13 18:16:31 -0500

    Basic array
    avengers name

--- Day 4/js/script.js
+++ Day 4/js/script.js
@@ -6,3 +6,45 @@
 
 //alert("Making sure this thing works.");
 
+//Lets create a basic array
+
+var avengers = ["Thor", "Captain America", "Iron Man"];
+
+//Print to console
+
+console.log(avengers);
+
+//Print one name to console
+
+console.log(avengers[1]);
+
+//Switching index 2 for spider-man
+
+avengers[2] = "Spider-man";
+
+console.log(avengers);
+
+//Adding a team member to the array
+
+avengers[3] = "Black Widow";
+
+console.log(avengers);
+
+//Use a variable as an index number
+
+var num = 2;
+
+console.log(avengers[num]);
+
+//Creating a length property
+// .Syntax is fancy for use a period
+
+console.log(avengers.length);
+
+//last index number is the length minus 1
+
+//Recruit a new team member
+
+avengers[avengers.length] = "Power Man";
+
+console.log(avengers);
