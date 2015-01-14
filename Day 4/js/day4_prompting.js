fa3581175a9c4def5249cddedfde736edde04b10
jacobnigh <jacobnigh@hotmail.com>
2015-01-13 19:56:48 -0500

    Prompting
    Learned uses of prompting

--- Day 4/js/day4_prompting.js
+++ Day 4/js/day4_prompting.js
@@ -5,4 +5,17 @@
  */
 
 //alert("testing");
+//Ask user for inout = prompt()
 
+//var name = prompt("Enter your year of birth.");
+//console.log("Your year of birth is " + name);
+
+//are of rectangle
+
+var length = prompt("Enter the length of your rectangle.");
+var width = prompt("Enter the width of your rectangle.");
+var area;
+
+area = length * width;
+
+console.log("The area of your rectangle is " + area + " according to the length of " + length + " and a width of " + width + ".");
