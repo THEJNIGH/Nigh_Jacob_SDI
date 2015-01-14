06eab923ab3ab50026f90c5c01cf449a0a68844f
jacobnigh <jacobnigh@hotmail.com>
2015-01-13 22:59:29 -0500

    Added comments
    Added comment

--- Nigh_Jacob_Expressions_Assignment/js/script.js
+++ Nigh_Jacob_Expressions_Assignment/js/script.js
@@ -6,17 +6,16 @@
 
 //alert("testing");
 
-var longP = prompt("Please enter the length of the long parallel of your trapezoid:"); // asked for the length of the longest parallel
-longP = parseInt(longP);
-var shortP = prompt("Please enter the length of the short parallel:"); // asked for the length of the shortest parallel
-shortP = parseInt(shortP);
-var height = prompt("Please enter the height:"); // ask for the height
-height = parseInt(height);
-var peices = [longP, shortP, height]; // the array to hold the asked information
-var a
-console.log(peices);
+var longP = prompt("Please enter the length of the long parallel of your trapezoid:"); // Asked for the length of the longest parallel
+longP = parseInt(longP); // Changing the entered value to a number
+var shortP = prompt("Please enter the length of the short parallel:"); // Asked for the length of the shortest parallel
+shortP = parseInt(shortP); // Changing the entered value to a number
+var height = prompt("Please enter the height:"); // Ask for the height
+height = parseInt(height); // Changing the entered value to a number
+var peices = [longP, shortP, height]; // The array to hold the asked information
+peices[0] += peices[1]; // Use of first assignment operator to all the second piece of information to the first piece and the first piece holds that information
+var a = peices[0]; // Declared and defined a and made it equal to the first piece of the array
+a *= peices[2]; // Use of second assignment operator and took a and multiplied it to the last part of the array
+var total = a /2; // Last part declared and defined total and make it equal to a divided by 2
+console.log("The area of your trapezoid is: " + total); // Printed the statement. 
 
-peices[0] += peices[1];
-var a = peices[0];
-a *= peices[2];
-console.log(a);
