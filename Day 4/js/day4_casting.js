76caae824386c1669a8a6161f546477e2e9865e2
jacobnigh <jacobnigh@hotmail.com>
2015-01-13 20:45:54 -0500

    Learned about number casting
    Number()
    String()
    parseInt()

--- Day 4/js/day4_casting.js
+++ Day 4/js/day4_casting.js
@@ -6,3 +6,50 @@
 
 //alert("testing");
 
+//Casting = one variable to be another
+
+var stringVar = "6";
+var results = 7 + stringVar;
+console.log(results);
+
+//Number(variable or value to cast) - converts to number
+
+var castingResults = 7 + Number(stringVar);
+console.log(castingResults);
+
+//String(treated as a text string)
+
+var areaCode = 407;
+var firstDig = 203;
+var secondDig = 1234;
+
+var phoneNumber = "(" + areaCode + ")" + "-" + firstDig + "-" + secondDig;
+console.log(String(phoneNumber)); //One of the ways to do it
+console.log(String(areaCode) + String(firstDig) + secondDig); //Another way to do it.
+console.log(areaCode + firstDig + secondDig); // This is not how to do it.
+
+//Parsing an integer
+//ParseInt() looks threw a text string and returns a number
+//When doing parsing if there are multiple numbers it only returns the first number
+//The text string has to start with that number. However leading and trailing spaces are okay
+//If it cant find an integer it returns NaN
+//NaN means Not a Number
+
+var a = parseInt("19 years old");
+console.log(a);
+
+var b = parseInt("I am 19 years old");
+console.log(b);
+
+var c = Number("19 years old");
+console.log(c);
+
+//Prompts only return strings.
+
+var shoesHad = prompt("How many shoes did you own before?");
+var shoesBought = prompt("How many pairs of shoes did you buy?");
+
+//Add shoes bought with shoes had for total number of shoes.
+
+var totalShoes = parseInt(shoesHad) + parseInt(shoesBought);
+console.log(totalShoes);
