48229e3ce6a4961b30da56f90a4a63df1541a725
jacobnigh <jacobnigh@hotmail.com>
2015-01-15 19:43:56 -0500

    Else if
    testing conditions with else if

--- Day 5/js/script.js
+++ Day 5/js/script.js
@@ -19,7 +19,11 @@ Basic structure of if statement
 if(Condition to test)
  {
   runs if the condition is true;
- };
+ }
+else
+ {
+  runs if the condition above is false.
+ }
  */
 if(oldEnough)
 {
@@ -35,10 +39,13 @@ console.log("What if you skip all of this.");
 
 // If the kid is over 48 inches tall then he can ride.
 
-var kidHeight = 20;
-var minHeight =48;
+var kidHeight = 47;
+var minHeight = 48;
+var shoeLift = 2;
+// Is the child tall enough with shoe lift?
 
-if(kidHeight > minHeight)
+
+if(kidHeight + shoeLift > minHeight)
 {
  console.log("You are tall enough to ride the roller coaster.");
 }
@@ -47,3 +54,42 @@ else
  // This code will run if the the test is false.
  console.log("Im sorry, you are not tall enough to ride this ride.");
 }
+
+// What ride can the kid go on based on his height.
+// if kid is 48 space mountain
+// if kid is 40 buzz light year ride
+// anything shorter Winnie the pooh.
+
+var childHeight = 42;
+
+if(childHeight >= 48)
+{
+ console.log("You can ride Space Mountain, Buzz Lightyear, and Winnie the Pooh")
+}
+else if(childHeight >= 40)
+{
+ console.log("You can ride the Buzz Lightyear, and Winnie the Pooh.")
+}
+else
+{
+ console.log("You are not tall enough to ride all the other rides so you must ride the Winnie the Pooh ride.")
+};
+
+/*
+if
+else if
+else statement
+
+if(condition1)
+{
+ code to run if true.
+}
+else if(condition2)
+{
+ code if first condition is false.
+}
+else
+{
+ catch all
+};
+ */
