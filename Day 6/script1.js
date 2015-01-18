9a4dbdaeaae48d2bc95fb8233c571d88aab14ff6
jacobnigh <jacobnigh@hotmail.com>
2015-01-18 11:44:23 -0500

    scratched idea came up with new one
    Finally came up with a good idea

--- Day 6/script1.js
+++ Day 6/script1.js
@@ -5,3 +5,28 @@
 */
 
 //alert("JavaScript works!");
+
+//You have $5,000.00 dollars in your bank account and you are checking with your bank to see if you can afford that item.
+
+var item1 = prompt("How much is your first item?");
+var item2;
+var item3;
+
+if (item1 <= 5000)
+{
+    console.log("Yes, you can afford this item.");
+        
+    {
+        item2 = prompt("How much is your second item");
+        item2 = parseInt(item2);
+        
+        if (item2 <= item1 - 5000)
+        {
+            console.log("Yes, you can afford your second item");
+        }
+        else
+        {
+            console.log("Im sorry, you cannont afford this item.");
+        }
+    }
+}
--- Nigh_Jacob_Conditionals/script1.js
+++ Nigh_Jacob_Conditionals/script1.js
@@ -5,3 +5,15 @@
 */
 
 //alert("JavaScript works!");
+
+var suitPrice1 = prompt("Enter the first amount of the first suit: ");
+if (suitPrice1 == "" || NaN)
+{
+    alert("You have not entered a price for your first suit, please try again.");
+    suitPrice1 = prompt("Enter the first amount of the first suit: ");
+}
+else
+{
+    suitPrice1 = parseInt(suitPrice1);
+    Console.log("This is your first suit price");
+}
