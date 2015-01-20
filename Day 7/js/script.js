d45d5a22f3d6172c2f1ef47a81ce84753d024cdf
jacobnigh <jacobnigh@hotmail.com>
2015-01-20 17:39:22 -0500

    Beginning files for while loop
    created files

---
+++ Day 7/js/script.js
@@ -0,0 +1,8 @@
+/*
+    *Jan 17 1015
+    *Jacob Nigh
+    *Nigh_Jacob_While loops
+*/
+
+//alert("JavaScript works!");
+
--- Nigh_Jacob_Conditionals/script1.js
+++ Nigh_Jacob_Conditionals/script1.js
@@ -11,11 +11,8 @@
 var suitPrice = 800;
 var suitPrice2 = 400;
 var budget = prompt("How big is your budget?");
-budget = parseInt(budget);
 var bill1 = prompt("What is the price of the first bill?");
-bill1 = parseInt(bill1);
 var bill2 = prompt("What is the price of the second bill?");
-bill2 = parseInt(bill2);
 
 var money = [budget, bill1, bill2];
 var billTotal = money[1] + money[2];
@@ -23,6 +20,42 @@ var total = money[0] - billTotal;
 var final = total - suitPrice;
 var final2 = total - suitPrice2;
 var suitTotal = suitPrice + suitPrice2;
+
+if (budget === "")
+{
+    alert("Please enter the amount again.");
+    budget = prompt("How big is your budget: ");
+    
+    if (bill1)
+    {
+        alert("Please enter the amount agian.");
+        bill1 = prompt("What is the price of uour first bill: ");
+    
+        if (bill2)
+        {
+            alert("Please enter the amount agian.");
+            bill2 = prompt("What is the price of your second bill: ");
+        }
+    }
+}
+
+/*if (budget === "")
+{
+    alert("Please enter the amount again.");
+    budget = prompt("How big is your budget: ");
+}
+else if (bill1 === "")
+{
+    alert("Please enter the amount agian.");
+    bill1 = prompt("What is the price of uour first bill: ");
+}
+
+else if (bill2 === "")
+{
+    alert("Please enter the amount agian.");
+    bill2 = prompt("What is the price of your second bill: ");
+}
+*/
 
 
 (final >= 400 && final <=799)?console.log("You can purchase the second suit which is $" + suitPrice2 + "."):console.log("You might be able to purchase the more expensive suit, lets have a look.");
