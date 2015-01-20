c79fb711d456132e8faf11107c2c74021402834a
jacobnigh <jacobnigh@hotmail.com>
2015-01-20 18:50:35 -0500

    for loops
    learned breaks

--- Day 7/js/script1.js
+++ Day 7/js/script1.js
@@ -4,4 +4,32 @@
     *Nigh_Jacob_for loops
 */
 
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Basic for loop structure
+
+/*
+ *for(initiazation; test; )
+ *{
+ *   condition to test;
+ *   incerement of change++;
+ *
+ *  code to run as long as the condition is true
+ *}
+ */
+
+for (var i = 0; i < 20; i += 4)
+{
+    //code to run as long as less than 20
+    console.log("The value of i = " + i);
+}
+
+for (var k = 0; k < 5; k++)
+{
+    console.log("The value of k is = " + k);
+    
+    if (k === 3)
+    {
+        break;
+    }
+}
