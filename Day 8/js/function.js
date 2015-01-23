ade6b9a07101408fb7816f5bb7b20221d50a95c7
jacobnigh <jacobnigh@hotmail.com>
2015-01-22 19:40:41 -0500

    Functions and scope
    creatd basic files

---
+++ Day 8/js/function.js
@@ -0,0 +1,38 @@
+/*
+ *Jacob Nigh
+ *Jan 22, 2015
+ *Validating
+ */
+
+//alert("Testing");
+
+//function functionNam(){code to be run};
+
+//function that logs onto the console hello
+
+function printHello()
+{
+    console.log("Hello.");
+}
+//called the function to do what it needs to do.
+printHello();
+printHello();
+
+//function to print lore text
+function printMore()
+{
+    console.log("Inside of print more and here is more text.");
+}
+printMore();
+
+//create a function that calculates of are of rectangle
+
+function areaRect()
+{
+    //create variables for width height and area
+    var height = 20;
+    var width = 30;
+    var area = height * width;
+    console.log("The area of the rectangle is " + area)
+}
+areaRect();
--- Day 8/js/scope.js
+++ Day 8/js/scope.js
@@ -5,3 +5,26 @@
  */
 
 //alert("Testing");
+
+//variable scope
+//variables inside and outside of a function
+//try not to use the same variable names
+//however in large files this is going to be impossible
+//variables created inside of fucntions call only be seen inside of those functions
+
+//variable for width in our main code.
+
+var width = 5; //scoped outside of the function for main code
+//create a functions that calculates the primeter of a rectangle.
+
+function calcPeri()
+{
+    var width = 10;//it scoped to the function calcPri
+    console.log("Inside of the function the value of width is " + width);
+    var height = 20;
+    var perimeter = width * 2 + height * 2;
+    console.log("Inside of function the perimeter is " + perimeter);
+}
+console.log("Before call " + width);
+calcPeri();
+console.log();
