dceaa2468fa1dad834993c3559de44bd6c0d30bd
jacobnigh <jacobnigh@hotmail.com>
2015-01-22 22:05:44 -0500

    Started and finished part 1 circumference
    finished and it works

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -4,4 +4,20 @@
  *Nigh_Jacob_Functions_Worksheet
  */
 
-alert("Testing");
+//alert("Testing");
+
+//Circumference
+
+var radii = prompt("What is the radius of your circle?")
+while(isNaN(radii) || radii === "")
+{
+    radii = prompt("Please enter an amount. What is the radius of your cricle?")
+}
+function circleC(r)
+{
+    var total = 2 * Math.PI * r;
+    return total;
+}
+radii = parseInt(radii);
+var totalArea = circleC(radii);
+console.log(totalArea);
