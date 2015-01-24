6859294283ccfb19f96b98457b32838689656dc1
jacobnigh <jacobnigh@hotmail.com>
2015-01-24 18:33:25 -0500

    starting multiple finished procedure
    created basic files

---
+++ Day 9/js/multiple.js
@@ -0,0 +1,7 @@
+/*
+ *Jacob Nigh
+ *Jan 24, 2015
+ *Multiple
+ */
+
+//alert("Testing");
--- Day 9/js/procedure.js
+++ Day 9/js/procedure.js
@@ -5,3 +5,24 @@
  */
 
 //alert("Testing");
+
+// This is a basic function
+function calcArea(w, h)
+{
+    var area = w * h;
+    return area;
+}
+var results = calcArea(4, 5);
+console.log(results);
+
+//This is a procedure and it does not return a value pretty much a list of steps.
+function calcAreaP(w, h)
+{
+    //calculations
+    var area = w * h;
+    console.log(area);
+    //It will not have a return.
+}
+
+//fucntion call for a procedure.
+calcAreaP(6, 7);
